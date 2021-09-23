eeac3ce584b7c1eef539bc685f151485dc930217
qbaby <lzhp1988@gmail.com>
2021-09-23 23:19:56 +0800

    use viper load config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,11 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/lzhp1988/simplebank/util"
 )
 
 var testQueries *Queries
@@ -19,7 +15,12 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource) //cannot use := , why?
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load configs", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource) //cannot use := , why?
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
